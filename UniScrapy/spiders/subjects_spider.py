ded2a8a18d482c6a87b9e39cc88884d13060bd02
Yunfei Jing <jinyj@student.unimelb.edu.au>
2020-03-19 20:08:12 +1100

    crawled basic info for comp-related subjects

--- UniScrapy/spiders/subjects_spider.py
+++ UniScrapy/spiders/subjects_spider.py
@@ -4,21 +4,38 @@ import scrapy
 class SubjectsSpider(scrapy.Spider):
     name = "subjects"
     start_urls = [
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30022/eligibility-and-requirements'
+        'https://handbook.unimelb.edu.au/2020/subjects/comp30022',
+        'https://handbook.unimelb.edu.au/2020/subjects/comp30026',
+        'https://handbook.unimelb.edu.au/2020/subjects/swen30006',
+        'https://handbook.unimelb.edu.au/2020/subjects/comp30023'
     ]
 
     def parse(self, response):
+
         name = response.css('title::text').get().split(' ')
         yield {
-            # Need to extract the subject name and code
-            'subject': ' '.join(name[3:-7]),
+            # Extract the subject information
+            'subject': ' '.join(name[0:-7]),
             'code': name[-7][1:-1],
-            'availability': None
+            'overview': response.css('div.course__overview-wrapper p::text').getall()[0],
+            'availability': response.css('div.course__overview-box table tr td div::text').getall()
         }
 
-        next_page = response.css('#prerequisites table.zebra > tr > td a::attr(href)').get()
+        # Get the 'eligibility and requirements page'
+        next_page = response.css('div.layout-sidebar__side__inner ul li a::attr(href)').getall()[1]
         
         if next_page is not None:
-            # Crawling the prerequisites page
-            next_page = response.urljoin(str(next_page)+'/eligibility-and-requirements')
-            yield scrapy.Request(next_page, callback=self.parse)
+            # Pass the requirement page to parseReq function
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parseReq)
+
+
+    def parseReq(self, response):
+
+        # Get all prerequisites 
+        pages = response.css('#prerequisites table.zebra > tr > td a::attr(href)').getall()
+
+        for next_page in pages:
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
