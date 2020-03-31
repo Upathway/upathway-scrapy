4216ce4f5aa4ce0972f2b8176412f0988015daa8
Yunfei Jing <jinyj@student.unimelb.edu.au>
2020-03-31 23:00:41 +1100

    new crawing method

--- UniScrapy/spiders/subjects_spider.py
+++ UniScrapy/spiders/subjects_spider.py
@@ -5,26 +5,31 @@ from UniScrapy.items import UniscrapyItem
 class SubjectsSpider(scrapy.Spider):
     name = "subjects"
     start_urls = [
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30022',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30026',
-        'https://handbook.unimelb.edu.au/2020/subjects/swen30006',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30023',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30027',
-        'https://handbook.unimelb.edu.au/2020/subjects/mast30025',
-        'https://handbook.unimelb.edu.au/2020/subjects/mast30027',
-        'https://handbook.unimelb.edu.au/2020/subjects/mast30034',
-        'https://handbook.unimelb.edu.au/2020/subjects/mast30021',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30028',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30001',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30030',
-        'https://handbook.unimelb.edu.au/2020/subjects/comp30031'
+        # All undergraduate CIS subjects
+        'https://handbook.unimelb.edu.au/search?area_of_study%5B%5D=all&campus_and_attendance_mode%5B%5D=all&org_unit%5B%5D=4180&sort=_score%7Cdesc&study_periods%5B%5D=all&subject_level_type%5B%5D=undergraduate&types%5B%5D=subject&year=2020'
     ]
 
     def parse(self, response):
 
-        name = response.css('title::text').get().split(' ')
-        sspost = UniscrapyItem()
+        # Get all subjects' relative links
+        subjects_list = response.css("div#search-results ul.search-results__list a::attr(href)").getall()
 
+        # join the links and then parse each subjects
+        for subject in subjects_list:
+            yield response.follow(subject, callback=self.parseSubject)
+        
+        next_page = response.urljoin(response.css("div#search-results div.search-context span.next a::attr(href)").get())
+
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        
+
+    def parseSubject(self, response):
+
+        name = response.css('title::text').get().split(' ')
+
+        sspost = UniscrapyItem()
 
         # Extract the subject information
         sspost['subject'] = ' '.join(name[0:-7])
@@ -32,8 +37,12 @@ class SubjectsSpider(scrapy.Spider):
         sspost['overview'] = response.css('div.course__overview-wrapper p::text').getall()[0]
         sspost['availability'] = response.css('div.course__overview-box table tr td div::text').getall()
 
+
         yield sspost
 
+        
+
+        '''
         # Get the 'eligibility and requirements page'
         next_page = response.css('div.layout-sidebar__side__inner ul li a::attr(href)').getall()[1]
         
@@ -41,14 +50,4 @@ class SubjectsSpider(scrapy.Spider):
             # Pass the requirement page to parseReq function
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parseReq)
-
-
-    def parseReq(self, response):
-
-        # Get all prerequisites 
-        pages = response.css('#prerequisites table.zebra > tr > td a::attr(href)').getall()
-
-        for next_page in pages:
-            if next_page is not None:
-                next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parse)
+        '''
